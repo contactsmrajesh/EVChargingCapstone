cd69cc6c91a42271461f370c5fee4fae78017039
Rajesh <contactsmrajesh@gmail.com>
2020-11-18 22:56:44 -0800

    Update EVCharging.sol
    Modified EV Wallet Functions

--- src/contracts/EVCharging.sol
+++ src/contracts/EVCharging.sol
@@ -1,62 +1,51 @@
-pragma solidity ^0.5.0;
-
-contract EVCharging{
-	string public name;
-	uint public evCustCount = 0;
-	mapping(uint => EVCustomer) public evCustomers;
-
-	struct EVCustomer {
-		uint custID;
-		uint walletAmount;
-		string custName;	
-		address owner;
-	}
-
-	event WalletCreated(
-		uint custID,
-		uint walletAmount,
-		string custName,	
-		address owner	
-	);
-
-	event WalletUpdated(
-		uint custID,
-		uint walletAmount,
-		string custName,	
-		address owner	
-	);
-
-	constructor() public {
-		name = "EVCharging Application";
-	}
-
-	function createWallet(string memory _custName, uint _walletAmount) public {
-		//Validate parameters start
-		require (bytes(_custName).length > 0 );
-		require(_walletAmount > 0);
-		//Validate parameters ends		
-		evCustCount ++;
-		evCustomers[evCustCount] = EVCustomer(evCustCount, _walletAmount, _custName, msg.sender);
-		//Events are used for logging data
-		emit WalletCreated(evCustCount, _walletAmount, _custName, msg.sender);
-	}
-
-	function topUpWallet(uint _custID) public payable{
-		//Validate the amount and custID
-		require(_custID > 0);
-		//Fetch the customer details. Instantiating new EVCustomer and creating a copy in memory for the ID.
-		EVCustomer memory _evCustomer = evCustomers[_custID];
-		//Update the wallet amount to the new amount
-		//_evCustomer.walletAmount = _evCustomer.walletAmount + _newAmount;
-		//Update the blockchain value with the new amount
-		_evCustomer.walletAmount += msg.value;
-		evCustomers[_custID] = _evCustomer;
-		emit WalletUpdated(evCustCount, _evCustomer.walletAmount, _evCustomer.custName, msg.sender);
-	} 
-
-	function queryWallet(uint _custID) public view returns(uint) {
-    	EVCustomer memory _evCustomer = evCustomers[_custID];		
-    	return _evCustomer.walletAmount;
-	}
-
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.6.0;
+contract EVWallet{
+    
+    struct payment{
+        uint amt;
+        uint timestamp;
+    }
+    
+    struct balance{
+        string customerName;
+        address custAccount;
+        uint totalBalance;
+        uint noOfPay;
+        mapping(uint => payment) payments;
+    }
+    
+    mapping(address => balance) balance_record;
+    
+    function createWallet(string memory _custName,uint _walletAmt) payable public{
+        balance_record[msg.sender].totalBalance +=  _walletAmt;
+        balance_record[msg.sender].customerName = _custName;
+        balance_record[msg.sender].custAccount = msg.sender;
+        balance_record[msg.sender].noOfPay += 1;
+        payment memory pay = payment(msg.value,now);
+        balance_record[msg.sender].payments[balance_record[msg.sender].noOfPay] = pay;
+    }
+    
+     function topUpWallet(uint _walletAmount) payable public{
+        balance_record[msg.sender].totalBalance +=  _walletAmount;
+        balance_record[msg.sender].noOfPay += 1;
+        payment memory pay = payment(msg.value,now);
+        balance_record[msg.sender].payments[balance_record[msg.sender].noOfPay] = pay;
+    }
+    
+    function queryWallet() view public returns(uint) {
+        return balance_record[msg.sender].totalBalance;
+    }
+    
+    
+    function getCustomer() view public returns(string memory) {
+        //name1 = balance_record[msg.sender].customerName;
+        return balance_record[msg.sender].customerName;
+    }
+    
+    function getCustomerAccount() view public returns(address) {
+        //name1 = balance_record[msg.sender].customerName;
+        return balance_record[msg.sender].custAccount;
+    }
+        
 }
